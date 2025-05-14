3876430b36049bf881d56d755de931a13692e4a4
Sphephelo Ndimande <SphepheloN@hollywoodbets.net>
2025-05-14 11:40:47 +0200

    Adding Real Data Repositories Implementation
    1. Create and implement 'PieRepository' and 'CategoryRepository'.
    2. Update repository dependency injections by replacing 'mock' repositories with the newly implemented ones in 'Program.cs'

---
+++ BethanysPieShop/Models/Repositories/CategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BethanysPieShop.Models.Repositories;
+
+public class CategoryRepository(
+    BethanysPieShopDbContext context) : ICategoryRepository
+{
+    public IEnumerable<Category> GetAllCategories => 
+        context.Categories.OrderBy(p => p.CategoryName);
+}
--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 
 builder.Services.AddControllersWithViews(); // Ensures that our app knows about ASP.NET Core MVC
 
