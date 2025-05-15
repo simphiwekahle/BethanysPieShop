6fe92b7481d0dd802f63a71dcb331780904b56e5
Sphephelo Ndimande <SphepheloN@hollywoodbets.net>
2025-05-15 13:00:53 +0200

    View Components Implementation
    *Shopping Cart view component
    1. Add and Implement 'ShoppingCartSummary' component
    2. Update '_ViewImports'
    3. Add and implement in Views Components/ShoppingCartSummary/Default
    4. Update _Layout to reference ShoppingCartSummary view component

---
+++ BethanysPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShop.Models.Repositories;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Components;
+
+public class ShoppingCartSummary(
+    IShoppingCart shoppingCart) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var items = shoppingCart.GetShoppingCartItems();
+        shoppingCart.ShoppingCartItems = items;
+
+        var shoppingCartViewModel = new ShoppingCartViewModel(
+            shoppingCart,
+            shoppingCart.GetShoppingCartTotal());
+
+        return View(shoppingCartViewModel);
+    }
+}
