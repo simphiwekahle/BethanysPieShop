f6a9230483f03d74bfdcd863c73c979d767d2ba6
Sphephelo Ndimande <SphepheloN@hollywoodbets.net>
2025-05-16 14:27:47 +0200

    View Component Implementation
    *Category Menuview component implementation
    1. Add and Implement 'CategoryMenu' component
    2. Add and implement in Views 'Components/CategoryMenu/Default'
    3. Update _Layout to reference CategoryMenu view component.
    
    *Client-Side Libraries
    1. Update the bootstrap library to include 'bootstrap.bundle.min.js'
    2. Update _Layout to reference 'bootstrap.bundle.min.js' instead of 'bootstrap.js'
    
    *PieController
    1. Update the 'List' action method to cator for the 'category' string parameter.

---
+++ BethanysPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShop.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Components;
+
+public class CategoryMenu(
+    ICategoryRepository categoryRepository) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var categories = categoryRepository.GetAllCategories.OrderBy(c => c.CategoryName);
+
+        return View(categories);
+    }
+}
--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,4 +1,5 @@
-﻿using BethanysPieShop.Models.Repositories;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.Models.Repositories;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,28 @@ public class PieController(
     IPieRepository pieRepository,
     ICategoryRepository categoryRepository) : Controller
 {
-    public IActionResult List()
+    public IActionResult List(string category)
     {
-        PieListViewModel pieListViewModel = new PieListViewModel(
-            pieRepository.GetAllPies,
-            "All Pies");
+        IEnumerable<Pie> pies;
+        string? currentCategory;
 
-        return View(pieListViewModel);
+        if (string.IsNullOrEmpty(category))
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel(
+                pieRepository.GetAllPies,
+                "All Pies");
+
+            return View(pieListViewModel);
+        }
+        else
+        {
+            pies = pieRepository.GetAllPies.Where(p => p.Category.CategoryName == category)
+                .OrderBy(p => p.PieId);
+            currentCategory = categoryRepository.GetAllCategories
+                .FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+        }
+        return View(new PieListViewModel (pies, currentCategory));
+        
     }
 
     public IActionResult Details(int id)
