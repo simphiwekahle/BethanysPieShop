71cae193ae2246dfc60ccffb3aa3f2c99162267d
Sphephelo Ndimande <SphepheloN@hollywoodbets.net>
2025-05-19 15:21:16 +0200

    Model Binding
    *Order Controller
    1. Add and implement the 'Checkout' post action method
    2. Add and implement the 'CheckoutComplete' action method
    3. Add and implement in Views Order/CheckoutComplete razor view

--- BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using BethanysPieShop.Models.Repositories;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.Models.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers;
@@ -9,6 +10,35 @@ public class OrderController(
 {
     public IActionResult Checkout()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        var items = shoppingCart.GetShoppingCartItems();
+        shoppingCart.ShoppingCartItems = items;
+
+        if (shoppingCart.ShoppingCartItems.Count == 0)
+        {
+            ModelState.AddModelError("", "Your cart is empty, add some pies first");
+        }
+
+        if (ModelState.IsValid)
+        {
+            orderRepository.CreateOrder(order);
+            shoppingCart.ClearCart();
+
+            return RedirectToAction("CheckoutComplete");
+        }
+
+        return View(order);
+    }
+
+    public IActionResult CheckoutComplete()
+    {
+        ViewBag.CheckoutCompleteMessage = "Thanks for the order. You'll soon be enjoying your delicious pie";
+
         return View();
     }
 }
