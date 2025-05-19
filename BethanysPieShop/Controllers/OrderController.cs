c3301f02f8694d71270dde16d48eb231df6c277d
Sphephelo Ndimande <SphepheloN@hollywoodbets.net>
2025-05-19 14:53:46 +0200

    Order Checkout Implementation
    *Creating Order Form
    1. Add and implement 'Order and OrderDetail' models
    2. Add and implemement the following:
    2.1. 'IOrderRepository' interface
    2.2. 'OrderRepository' inheriting the IOrderRepository interface
    2.3. Register the Order Repository interface and implementation for dependecy injection in 'Program.cs'
    3. Update the DbContext implementation to cater for the Order and OrdeDetails
    4. Run the migration commands using package manager console to update the database
    5. Add and implement the 'OrderController'
    6. Add and implement in Views Order/Checkout form
    7. Update in Views ShoppingCart/Index by adding the 'Check Out Now' button

---
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShop.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class OrderController(
+    IOrderRepository orderRepository,
+    IShoppingCart shoppingCart) : Controller
+{
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+}
